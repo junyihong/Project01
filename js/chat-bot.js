0c73d90e9c1fe8ed5d1baac1545bc7df47149dc7
cjwo-n <whwjddnjs0@naver.com>
2023-03-13 21:40:14 +0900

    help me

--- js/chat-bot.js
+++ js/chat-bot.js
@@ -1,9 +1,19 @@
 $(document).ready(function () {
-    $(".text__icon").hide();
-    $("button.bot").click(function () {
-      $(".text__icon").slideDown();
-    });
-    $("#chat__top__btn").click(function(){
-      $(".text__icon").slideUp();
-    })
+  $(".text__icon").hide();
+  $("button.bot").click(function () {
+    $(".text__icon").slideDown();
   });
+  $("#chat__top__btn").click(function () {
+    $(".text__icon").slideUp();
+  });
+});
+//product_detail alone
+$(document).ready(function () {
+  $(".text__icon__pidAlone").hide();
+  $("button.bot").click(function () {
+    $(".text__icon__pidAlone").slideDown();
+  });
+  $("#chat__top__btn").click(function () {
+    $(".text__icon__pidAlone").slideUp();
+  });
+});
